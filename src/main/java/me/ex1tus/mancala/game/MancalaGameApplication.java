0d2f97a35fdc5e38231a41638d8ded9500e1cba6
Alexander Romanov <ex1tus@yandex.ru>
2023-10-25 13:16:50 +0400

    Initial commit

---
+++ src/main/java/me/ex1tus/mancala/game/MancalaGameApplication.java
@@ -0,0 +1,13 @@
+package me.ex1tus.mancala.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MancalaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MancalaGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ex1tus/mancala/game/MancalaGameApplicationTests.java
@@ -0,0 +1,12 @@
+package me.ex1tus.mancala.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MancalaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
